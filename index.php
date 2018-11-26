0aa66578e61e7e8d165f80c5fb257d6f4e3ae4bc
khoursaaf <khoursa@gmail.com>
2018-11-26 16:50:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL existent et les afficher: index.php?lastname=Nemare&firstname=Jean</p>
+    <a href="index.php?lastname=Nemare&amp;firstname=Jean">Dis-moi bonjour !</a>
+  </body>
+</html>
