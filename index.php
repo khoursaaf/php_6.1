bbe6ff7c26b264b22c027bee69f178a4e79c7758
khoursaaf <khoursa@gmail.com>
2018-11-27 15:16:40 +0100

    correction

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Exercice</title>
   </head>
   <body>
     <p>Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL existent et les afficher: index.php?lastname=Nemare&firstname=Jean</p>
-    <a href="index.php?lastname=Nemare&amp;firstname=Jean">Dis-moi bonjour !</a>
+    <a href="index.php?lastname=Nemare&amp;firstname=Jean">Afficher dans url!</a>
+    <?php
+    if (isset($_GET['lastname']) && isset($_GET['firstname'])) { ?>
+    <p>
+      <?= 'Les paramètres sont bien présent.'; ?>
+    </p>
+    <?php
+    }
+    else {
+    ?>
+    <p>
+      <?= 'Les paramètres ne sont pas présent'; }?>
+    </p>
   </body>
 </html>
